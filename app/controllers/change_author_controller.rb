fd8e33db5df9cf3142ff7c24fb7b4a01316a766d
Josef Cech <czech.josef@gmail.com>
2012-11-16 17:50:20 +0100

    JournalDetail is saved on author change

--- app/controllers/change_author_controller.rb
+++ app/controllers/change_author_controller.rb
@@ -10,8 +10,18 @@ class ChangeAuthorController < ApplicationController
 
   def edit
     issue = Issue.find_by_id params[:id]
-    issue.author = User.find_by_id params[:author_id]
-    issue.save
+    old_author = issue.author
+    new_author = User.find_by_id params[:author_id]
+
+    if old_author != new_author
+      issue.author = new_author
+      journal = issue.init_journal(User.current)
+      journal.details << JournalDetail.new(:property => 'attr',
+                                           :prop_key => :author,
+                                           :old_value => old_author.name,
+                                           :value => new_author.name)
+      issue.save
+    end
     redirect_to issue_path(issue)
   end
 end
